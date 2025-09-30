1cc3cf0463aa1a2818a7dc9549e66b52be48a57b
Cezar Maldini <cezar.datascience@gmail.com>
2025-09-30 18:04:01 -0300

    Modelo colbert

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ class Settings(BaseSettings):
     dense_model_name: str = ("sentence-transformers/all-MiniLM-L6-v2")
     dense_model_max_tokens: int = 384
     bm25_model_name: str = "Qdrant/bm25"
-    late_interaction_model_name: str = "colbert-ir/colbertv2.0-mini"
+    late_interaction_model_name: str = "colbert-ir/colbertv2.0"
 
     # Postgres Supabase
     postgres_user: str = os.getenv('POSTGRES_USER')
